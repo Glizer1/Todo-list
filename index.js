c1464713e96115e37da4c6f67af6f65c62abe5d8
Glizer1 <gigio.sdoin@gmail.com>
2023-11-02 14:46:41 -0300

    update in DOM function

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 let tasksDb = [
-    { id: 0, name: "Task 1", checked: false, sectionId: 0 },
-    { id: 1, name: "Task 2", checked: true, sectionId: 0 },
-    { id: 2, name: "Task 1", checked: false, sectionId: 1 }
+    { id: '0', name: "Task 1", checked: false, sectionId: '0' },
+    { id: '1', name: "Task 2", checked: true, sectionId: '0' },
+    { id: '2', name: "Task 1", checked: false, sectionId: '1' }
 ]
 
 let sectionsDb = [
-    { id: 0, sectionName: "Section 1" },
-    { id: 1, sectionName: "Section 2" }
+    { id: '0', sectionName: "Section 1" },
+    { id: '1', sectionName: "Section 2" }
 ]
 
 // -- backEnd --
@@ -90,8 +90,18 @@ function generateId() {
 
 // -- frontEnd -- 
 
-function addTask(event) {
+// etapas para add Task
+/**
+ 1. clicar Add task: adiconar um preview de task com input para o nome
+ 2. verificar ao mandar input: verifica se o nome está dentro dos parametros
+ 3. Cria a task na DB: chama a funçao de criar task
+ */
+
+function addPreviewTask(event) {
     console.log(event)
+
+    const target = event.target
+    const section = target.closest('.project-section')
 
     const taskPreviewTemplate = `
         <div class="task">
@@ -102,17 +112,57 @@ function addTask(event) {
             </div>
         </div>
     `
-    const target = event.target
-    const taskList = target.parentElement.previousElementSibling
+    
+    const taskList = section.querySelector('.task-list')
     
     taskList.insertAdjacentHTML('beforeend', taskPreviewTemplate);
 
     const input = taskList.lastElementChild.querySelector('.task-name-input')
-    console.log(input)
     input.focus()
+
+    addEventListeners(input)
+}
+
+function addEventListeners(input) {
+    input.addEventListener('blur', handleEnterTaskName)
+    
+
+    input.addEventListener('keydown', (event) => {
+        const keyPressed = event.key
+    
+        if (keyPressed === 'Enter') {
+            handleEnterTaskName(event)
+        }
+    })
+}
+
+function removeEventListeners(input) {
+    input.removeEventListener('blur', handleEnterTaskName)
+}
+
+function handleEnterTaskName(event) {
+    const input = event.target
+    removeEventListeners(input)
+
+    const taskName = input.value.trim()
+
+    const sectionId = input.closest('.project-section').dataset.sectionid
+
+    if (taskName) {
+        createTask(taskName, sectionId)
+        
+        input.style.display = 'none' // Mudar: add in css
+    } else {
+        const taskList = input.closest('.task-list')
+        const previewTask = taskList.lastElementChild
+        taskList.removeChild(previewTask)
+    }
+
 }
 
 const addTaskButton = document.querySelectorAll('.add-task-button')
 addTaskButton.forEach(button => {
-    button.addEventListener('click', addTask)
+    button.addEventListener('click', addPreviewTask)
 })
+
+
